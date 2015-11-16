b20ce64032d9e4c688792777b79b082127ff5ca4
NiharikaS8 <niharika@tinyowl.co.in>
2015-11-17 01:58:28 +0530

    Added project

--- app/app.js
+++ app/app.js
@@ -1,12 +1,62 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+var QuestionAnsApp = angular.module('QuestionAnsApp', ['firebase'] );
+QuestionAnsApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/questions', {
+      templateUrl: 'partials/question-list.html',
+      controller: 'QuestionList'
+    }).
+    when('/questions/new', {
+      templateUrl: 'partials/question-form.html',
+      controller: 'QuestionNew'
+    }).
+    when('/questions/:questionId', {
+  templateUrl: 'partials/question-form.html',
+  controller: 'QuestionDetail'
+}).
+    otherwise({
+      redirectTo: '/questions'
+    });
 }]);
+
+QuestionAnsApp.controller('QuestionList', ['$scope', '$firebase', function($scope, $firebase) {
+  var firebaseUrl = "https://vivid-heat-7872.firebaseio.com/questions";
+  $scope.questions = $firebase(new Firebase(firebaseUrl));
+  $scope.deleteQuestion = function(questionId) {
+  $scope.questions.$remove(questionId);
+}
+$scope.answerQuestion = function(question, selectedAnswer) {
+  question.answered = true;
+  question.correct = (question.answer === selectedAnswer);
+};
+}]);
+
+QuestionAnsApp.controller('QuestionNew', ['$scope', '$firebase', '$location', function($scope, $firebase, $location) {
+  $scope.question = {};
+
+  $scope.persistQuestion = function(question) {
+    var firebaseUrl = "https://vivid-heat-7872.firebaseio.com/questions";
+    $scope.questions = $firebase(new Firebase(firebaseUrl));
+    $scope.questions.$add(question).then(function(ref) {
+  $location.url('/questions');
+    });
+  };
+}]);
+
+QuestionAnsApp.controller('QuestionDetail', ['$scope', '$firebase', '$routeParams', function($scope, $firebase, $routeParams) {
+  var firebaseUrl = "https://vivid-heat-7872.firebaseio.com/questions/" + $routeParams.questionId;
+  $scope.question = $firebase(new Firebase(firebaseUrl));
+  $scope.persistQuestion = function(question) {
+    $scope.question.$update({
+      question: question.question,
+      option1: question.option1,
+      option2: question.option2,
+      option3: question.option3,
+      answer: question.answer
+    }).then(function(ref) {
+      $location.url('/questions');
+    });
+  };
+}]);
+
